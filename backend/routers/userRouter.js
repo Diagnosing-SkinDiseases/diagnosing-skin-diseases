d7bc1f3f0a098862bbf34f491c08d5991919d3e1
Sean Sollestre <sean.sollestre@gmail.com>
2024-02-14 13:50:29 -0800

    Add initial user routes

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const userRouter = express.Router();
+
+// Create
+userRouter.post("/user/create", (req, res) => {
+  const isEmpty = Object.keys(req.body).length === 0;
+  console.log(req.body);
+  let user;
+  if (isEmpty) {
+    user = "Empty";
+  } else {
+    user = req.body.user;
+  }
+  console.log(user);
+  res.send(`Create: ${user}`);
+});
+
+// Read
+userRouter.get("/user/read", (req, res) => {
+  let response = req.query.user ? `Hello ${req.query.user}!` : "Hello User!";
+  res.send(`Read: ${response}`);
+});
+
+// Update
+userRouter.patch("/user/update", (req, res) => {
+  const isEmpty = Object.keys(req.body).length === 0;
+  console.log(req.body);
+  let user;
+  if (isEmpty) {
+    user = "Empty";
+  } else {
+    user = req.body.user;
+  }
+  console.log(user);
+  res.send(`Update: ${user}`);
+});
+
+// Delete
+userRouter.delete("/user/delete", (req, res) => {
+  const isEmpty = Object.keys(req.body).length === 0;
+  console.log(req.body);
+  let user;
+  if (isEmpty) {
+    user = "Empty";
+  } else {
+    user = req.body.user;
+  }
+  console.log(user);
+  res.send(`Delete: ${user}`);
+});
+
+module.exports = userRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const userRouter = require("./routers/userRouter.js");
+
+// App configurations
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Post test");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+// User endpoints
+app.use("/api", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
