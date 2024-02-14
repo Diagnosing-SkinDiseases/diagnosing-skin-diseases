2ac67f2af03df31ef1683c754451967865920613
Sean Sollestre <sean.sollestre@gmail.com>
2024-02-14 13:54:52 -0800

    Connect router to main server

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const userRouter = require("./routers/userRouter.js");
+const glossaryRouter = require("./routers/glossaryRouter.js");
 
 // App configurations
 app.use(express.json());
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("Post test");
-});
-
+// App endpoints start here
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
@@ -18,6 +15,10 @@ app.get("/", (req, res) => {
 // User endpoints
 app.use("/api", userRouter);
 
+// Glossary endpoints
+app.use("glossary", glossaryRouter);
+
+// Server start
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
