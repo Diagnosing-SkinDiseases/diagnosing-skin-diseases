c47f3113b928294e2004f80b9d9d45681488d576
jaason-shi <jasons8002@gmail.com>
2024-02-15 10:38:33 -0800

    Create title and search component

---
+++ frontend/src/components/ResearchArticles/ResearchArticleHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ResearchArticles = () => {
+  return (
+    <section className="research-articles">
+      <h2 className="research-title">Research Articles</h2>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+      </div>
+    </section>
+  );
+};
+
+export default ResearchArticles;
