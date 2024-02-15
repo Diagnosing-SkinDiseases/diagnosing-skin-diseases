94fc2c111fdc358e8006fe8ff9f789cc1eb93ab5
jaason-shi <jasons8002@gmail.com>
2024-02-15 11:11:16 -0800

    change classname from col to sample

--- frontend/src/components/ResearchArticles/SampleArticles.jsx
+++ frontend/src/components/ResearchArticles/SampleArticles.jsx
@@ -3,22 +3,22 @@ import React from "react";
 const SampleWords = () => {
   return (
     <div className="sample-words">
-      <div className="col">
+      <div className="sample">
         <a href="#">Sample Article 1</a>
       </div>
-      <div className="col">
+      <div className="sample">
         <a href="#">Sample Article 2</a>
       </div>
-      <div className="col">
+      <div className="sample">
         <a href="#">Sample Article 3</a>
       </div>
-      <div className="col">
+      <div className="sample">
         <a href="#">Sample Article 4</a>
       </div>
-      <div className="col">
+      <div className="sample">
         <a href="#">Sample Article 5</a>
       </div>
-      <div className="col">
+      <div className="sample">
         <a href="#">Sample Article 6</a>
       </div>
     </div>
