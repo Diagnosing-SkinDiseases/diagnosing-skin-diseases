636df50c0b3f4b1ae98861843110058e626a409d
Sean Sollestre <sean.sollestre@gmail.com>
2024-02-15 23:01:08 -0800

    Add validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,18 +49,19 @@ const updateUser = async (req, res) => {
     return res.status(400).json({ error: "Invalid id" });
   }
 
-  const user = await User.findOneAndUpdate(
-    {
-      _id: ObjectId.createFromHexString(id),
-    },
-    { ...data }
-  );
-
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: ObjectId.createFromHexString(id) },
+      { ...data },
+      { runValidators: true }
+    );
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-
-  res.status(200).json(user);
 };
 
 // Delete user
