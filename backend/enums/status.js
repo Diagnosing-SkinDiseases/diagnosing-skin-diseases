0d9c930fe92f76f1ba29b5ea453aa3acefda3c42
Sean Sollestre <sean.sollestre@gmail.com>
2024-02-15 21:06:29 -0800

    Set up simple create user

---
+++ backend/enums/status.js
@@ -0,0 +1,4 @@
+const Status = {
+  PUBLISHED: "PUBLISHED",
+  UNPUBLISHED: "UNPUBLISHED",
+};
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const userRouter = express.Router();
+const User = require("../models/userModel");
 
 // Create
-userRouter.post("/user/create", (req, res) => {
-  const isEmpty = Object.keys(req.body).length === 0;
-  console.log(req.body);
-  let user;
-  if (isEmpty) {
-    user = "Empty";
-  } else {
-    user = req.body.user;
+userRouter.post("/user/create", async (req, res) => {
+  let { username, email, password } = req.body;
+
+  try {
+    const user = await User.create({ username, email, password });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-  console.log(user);
-  res.send(`Create: ${user}`);
 });
 
 // Read
