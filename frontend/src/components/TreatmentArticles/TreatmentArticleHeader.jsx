a1e0a3294f2a246a7dd0981321a56a7c1a707f82
jaason-shi <jasons8002@gmail.com>
2024-02-22 00:45:00 -0800

    update title header string

--- frontend/src/components/TreatmentArticles/TreatmentArticleHeader.jsx
+++ frontend/src/components/TreatmentArticles/TreatmentArticleHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TreatmentArticles = () => {
   return (
     <div className="treatment-articles">
-      <h2 className="treatment-title">Treatment Articles</h2>
+      <h2 className="treatment-title">Treatment</h2>
       <div className="search-bar">
         <input type="text" placeholder="Search..." />
       </div>
