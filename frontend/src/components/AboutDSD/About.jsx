89539bea96d8d05722273dc516bbf7e833445521
jaason-shi <jasons8002@gmail.com>
2024-02-22 13:43:33 -0800

    create about DSD page

---
+++ frontend/src/components/AboutDSD/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import MainContent from "./MainContent";
+import Sidebar from "./Sidebar";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h2 className="mt-4" style={{ color: "#007bff" }}>
+        About DSD
+      </h2>
+      <div className="row">
+        <Sidebar />
+        <MainContent />
+      </div>
+    </div>
+  );
+};
+
+export default About;
