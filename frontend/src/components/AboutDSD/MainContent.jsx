3847de3b4ec84616cad7c9b83a345b8e17991eb6
jaason-shi <jasons8002@gmail.com>
2024-02-22 13:43:04 -0800

    add hardcoded main content text area

---
+++ frontend/src/components/AboutDSD/MainContent.jsx
@@ -0,0 +1,52 @@
+// MainContent.js
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <div className="col-md-9 mt-4">
+      <div>
+        <h2>About the Program</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+          nulla ut dolor condimentum fermentum. Proin tincidunt vehicula massa
+          vel fringilla. Nullam efficitur odio vel mauris eleifend, ac vehicula
+          enim tincidunt. Aliquam erat volutpat. Vestibulum ante ipsum primis in
+          faucibus orci luctus et ultrices posuere cubilia Curae; Mauris pretium
+          condimentum eros, in lobortis velit congue sit amet. Integer sodales
+          magna id metus suscipit, et tempus sem sollicitudin. Phasellus at urna
+          eget ligula congue venenatis.
+        </p>
+        <p>
+          Nullam efficitur felis nec neque fermentum, id fringilla elit
+          vestibulum. Vestibulum nec urna varius, sollicitudin risus id,
+          placerat libero. Maecenas vel lectus risus. Nam tincidunt orci auctor
+          nibh vehicula, sed luctus nunc suscipit. Aenean volutpat sagittis
+          odio, id lobortis tortor. Ut dignissim interdum sem, a tempor libero
+          varius non. Nullam ac turpis ultricies, condimentum velit eu, sagittis
+          neque. Nulla facilisi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+          nulla ut dolor condimentum fermentum. Proin tincidunt vehicula massa
+          vel fringilla. Nullam efficitur odio vel mauris eleifend, ac vehicula
+          enim tincidunt. Aliquam erat volutpat. Vestibulum ante ipsum primis in
+          faucibus orci luctus et ultrices posuere cubilia Curae; Mauris pretium
+          condimentum eros, in lobortis velit congue sit amet. Integer sodales
+          magna id metus suscipit, et tempus sem sollicitudin. Phasellus at urna
+          eget ligula congue venenatis.
+        </p>
+        <p>
+          Nullam efficitur felis nec neque fermentum, id fringilla elit
+          vestibulum. Vestibulum nec urna varius, sollicitudin risus id,
+          placerat libero. Maecenas vel lectus risus. Nam tincidunt orci auctor
+          nibh vehicula, sed luctus nunc suscipit. Aenean volutpat sagittis
+          odio, id lobortis tortor. Ut dignissim interdum sem, a tempor libero
+          varius non. Nullam ac turpis ultricies, condimentum velit eu, sagittis
+          neque. Nulla facilisi.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
