8c9580156845967f48b12339480e0331f5262e8e
Sean Sollestre <sean.sollestre@gmail.com>
2024-02-22 13:18:23 -0800

    Modify jsdocs

--- frontend/src/components/SeanPrototypes/articleApiController.js
+++ frontend/src/components/SeanPrototypes/articleApiController.js
@@ -2,23 +2,38 @@ import axios from "axios";
 import apiUrl from "../../api";
 
 /**
- * Creates a promise for getting all articles from the database.
+ * Creates a promise for getting all articles from the database. \
+ * See comments inside function for the format of payloads.
  *
- * @returns - the promise for the api response
+ * @returns the promise for the api response
  */
 const apiGetAllArticles = () => {
+  /*
+    Response payload format: 
+    [
+    {
+    title: String,
+    content: [{
+        type: Enum String (ArticleContentType)
+        content: String
+    }],
+    status: Enum String (Status)
+    }
+    ]
+  */
   return axios.get(`${apiUrl}/article/read/all`);
 };
 
 /**
- * Creates a promise for creating an article for the database.
+ * Creates a promise for creating an article for the database. \
+ * See comments inside function for the format of payloads.
  *
  * @param {*} payload - the payload for the post request as an object
- * @returns - the promise for the api response
+ * @returns the promise for the api response
  */
 const apiCreateArticle = (payload) => {
   /*
-    payload format: 
+    Request payload format: 
     {
     title: String,
     content: [{
@@ -32,11 +47,24 @@ const apiCreateArticle = (payload) => {
 };
 
 /**
+ * Creates a promise for deleting an article for the database. \
+ * See comments inside function for the format of payloads.
  *
  * @param {*} targetId - the _id of the target article as a string
  * @returns - the promise for the api response
  */
 const apiDeleteArticle = (targetId) => {
+  /*
+    Response payload format: 
+    {
+    title: String,
+    content: [{
+        type: Enum String (ArticleContentType)
+        content: String
+    }],
+    status: Enum String (Status)
+    }
+  */
   return axios.delete(`${apiUrl}/article/delete`, { data: { id: targetId } });
 };
 
