dc456a8d0afaa4ab1024ccb840dcc2f8fac6a30f
Shuyi Liu <shuyiliu95@gmail.com>
2024-03-05 22:17:33 -0800

    Create large dummy tree data.

--- frontend/src/components/UserTree/DummyTree.js
+++ frontend/src/components/UserTree/DummyTree.js
@@ -1,83 +1,195 @@
 const DUMMY_DATA = {
     "_id": "65cf3632790be1df5b052ce4",
-    "name": "Lesion",
+    "name": "Diagnostic Tree",
     "nodes": [
         {
             "currentId": "node1",
-            "content": "The question for node 1?",
+            "content": "Question for node 1?",
             "parentId": null,
             "noChildId": "node2",
             "yesChildId": "node3",
-            "_id": "65cf3632790be1df5b052ce5"
+            "_id": "unique_id_node1"
         },
         {
             "currentId": "node2",
-            "content": "The question for node 2?",
+            "content": "Question for node 2?",
             "parentId": "node1",
-            "noChildId": "node5",
-            "yesChildId": "node4",
-            "_id": "65cf3632790be1df5b052ce6"
-        },
-        {
-            "currentId": "node4",
-            "content": "The question for node 4?",
-            "parentId": "node2",
-            "noChildId": null,
-            "yesChildId": null,
-            "_id": "65cf3632790be1df5b052ce7"
+            "noChildId": "node4",
+            "yesChildId": "node5",
+            "_id": "unique_id_node2"
         },
         {
             "currentId": "node3",
-            "content": "The question for node 3?",
+            "content": "Question for node 3?",
             "parentId": "node1",
             "noChildId": "node6",
             "yesChildId": "node7",
-            "_id": "65cf3632790be1df5b052ce8"
+            "_id": "unique_id_node3"
+        },
+        {
+            "currentId": "node4",
+            "content": "Question for node 4?",
+            "parentId": "node2",
+            "noChildId": "node8",
+            "yesChildId": "node9",
+            "_id": "unique_id_node4"
         },
         {
             "currentId": "node5",
-            "content": "The question for node 5?",
+            "content": "Question for node 5?",
             "parentId": "node2",
-            "noChildId": null,
-            "yesChildId": "node8",
-            "_id": "65cf3632790be1df5b052ce9"
+            "noChildId": "node10",
+            "yesChildId": "node11",
+            "_id": "unique_id_node5"
         },
         {
             "currentId": "node6",
-            "content": "The question for node 6?",
+            "content": "Question for node 6?",
             "parentId": "node3",
-            "noChildId": null,
-            "yesChildId": null,
-            "_id": "65cf3632790be1df5b052cea"
+            "noChildId": "node12",
+            "yesChildId": "node13",
+            "_id": "unique_id_node6"
         },
         {
             "currentId": "node7",
-            "content": "The question for node 7?",
+            "content": "Question for node 7?",
             "parentId": "node3",
-            "noChildId": null,
-            "yesChildId": "node9",
-            "_id": "65cf3632790be1df5b052ceb"
+            "noChildId": "node14",
+            "yesChildId": "node15",
+            "_id": "unique_id_node7"
         },
         {
             "currentId": "node8",
-            "content": "The question for node 8?",
-            "parentId": "node5",
+            "content": "Question for node 8?",
+            "parentId": "node4",
             "noChildId": null,
             "yesChildId": null,
-            "_id": "65cf3632790be1df5b052cec"
+            "_id": "unique_id_node8"
         },
         {
             "currentId": "node9",
-            "content": "The question for node 9?",
+            "content": "Question for node 9?",
+            "parentId": "node4",
+            "noChildId": null,
+            "yesChildId": null,
+            "_id": "unique_id_node9"
+        },
+        {
+            "currentId": "node10",
+            "content": "Question for node 10?",
+            "parentId": "node5",
+            "noChildId": null,
+            "yesChildId": null,
+            "_id": "unique_id_node10"
+        },
+        {
+            "currentId": "node11",
+            "content": "Question for node 11?",
+            "parentId": "node5",
+            "noChildId": null,
+            "yesChildId": null,
+            "_id": "unique_id_node11"
+        },
+        {
+            "currentId": "node12",
+            "content": "Question for node 12?",
+            "parentId": "node6",
+            "noChildId": "node16",
+            "yesChildId": "node17",
+            "_id": "unique_id_node12"
+        },
+        {
+            "currentId": "node13",
+            "content": "Question for node 13?",
+            "parentId": "node6",
+            "noChildId": "node18",
+            "yesChildId": "node19",
+            "_id": "unique_id_node13"
+        },
+        {
+            "currentId": "node14",
+            "content": "Question for node 14?",
             "parentId": "node7",
             "noChildId": null,
             "yesChildId": null,
-            "_id": "65cf3632790be1df5b052ced"
-        }
+            "_id": "unique_id_node14"
+        },
+        {
+            "currentId": "node15",
+            "content": "Question for node 15?",
+            "parentId": "node7",
+            "noChildId": null,
+            "yesChildId": null,
+            "_id": "unique_id_node15"
+        },
+        {
+            "currentId": "node16",
+            "content": "Question for node 16?",
+            "parentId": "node12",
+            "noChildId": null,
+            "yesChildId": null,
+            "_id": "unique_id_node16"
+        },
+        {
+            "currentId": "node17",
+            "content": "Question for node 17?",
+            "parentId": "node12",
+            "noChildId": null,
+            "yesChildId": null,
+            "_id": "unique_id_node17"
+        },
+        {
+            "currentId": "node18",
+            "content": "Question for node 18?",
+            "parentId": "node13",
+            "noChildId": null,
+            "yesChildId": null,
+            "_id": "unique_id_node18"
+        },
+        {
+            "currentId": "node19",
+            "content": "Question for node 19?",
+            "parentId": "node13",
+            "noChildId": null,
+            "yesChildId": "node20",
+            "_id": "unique_id_node19"
+        },
+        {
+            "currentId": "node20",
+            "content": "Question for node 20?",
+            "parentId": "node19",
+            "noChildId": null,
+            "yesChildId": "node21",
+            "_id": "unique_id_node20"
+        },
+        {
+            "currentId": "node21",
+            "content": "Question for node 21?",
+            "parentId": "node20",
+            "noChildId": null,
+            "yesChildId": "node22",
+            "_id": "unique_id_node21"
+        },
+        {
+            "currentId": "node22",
+            "content": "Question for node 22?",
+            "parentId": "node21",
+            "noChildId": null,
+            "yesChildId": "node23",
+            "_id": "unique_id_node22"
+        },
+        {
+            "currentId": "node23",
+            "content": "Question for node 23?",
+            "parentId": "node22",
+            "noChildId": null,
+            "yesChildId": "node24",
+            "_id": "unique_id_node23"
+        },
     ],
-    "about": "Sample",
+    "about": "Sample diagnostic tree",
     "status": "PUBLISHED",
     "__v": 0
-}
+};
 
 export default DUMMY_DATA;
