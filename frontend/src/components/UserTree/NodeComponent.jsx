9b02bb1ee16de15548dbfe312cb75e271032712e
Shuyi Liu <shuyiliu95@gmail.com>
2024-03-20 23:47:23 -0700

    Add event listener to Node Component.

--- frontend/src/components/UserTree/NodeComponent.jsx
+++ frontend/src/components/UserTree/NodeComponent.jsx
@@ -4,12 +4,13 @@ import './NodeComponent.css';
 
 /* TODO: Input id and color for node to be rendered */
 /* TODO: Use js to change color of node if node is current node or being viewed information*/
-const NodeComponent = ({ color, id }) => {
+const NodeComponent = ({ color, id, onClick }) => {
     return (
-        <div className="node-component" id={id} style={{ backgroundColor: color }}>
+        <div className="node-component" id={id} style={{ backgroundColor: color }} onClick={() => onClick(id)}>
         </div>
     );
-}
+};
+
 
 const InvisibleNodeComponent = () => {
     return (
