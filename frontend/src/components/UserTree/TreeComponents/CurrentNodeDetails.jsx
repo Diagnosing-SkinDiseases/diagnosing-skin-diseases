3fa545cd2c570fd4cd204b3c704334ca92a76584
Shuyi Liu <shuyiliu95@gmail.com>
2024-03-28 16:40:46 -0700

    Change node size according to client feedback.

--- frontend/src/components/UserTree/TreeComponents/CurrentNodeDetails.jsx
+++ frontend/src/components/UserTree/TreeComponents/CurrentNodeDetails.jsx
@@ -5,13 +5,13 @@ import './CurrentNodeDetails.css';
 const CurrentNodeDetails = ({ question, onBack, onNo, onYes, showBack, showNo, showYes }) => {
     return (
         <div className="current-node-details">
-            <div className="question">
-                {question}
-            </div>
             <div className="buttons">
                 {showBack && <button onClick={onBack} className="btn btn-primary btn-back">Back</button>}
                 {showNo && <button onClick={onNo} className="btn btn-danger btn-no">No</button>}
                 {showYes && <button onClick={onYes} className="btn btn-success btn-yes">Yes</button>}
+            </div>
+            <div className="question">
+                {question}
             </div>
         </div>
     );
