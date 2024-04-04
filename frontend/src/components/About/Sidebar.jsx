6d7ce44ab64da1709fe03afa5703285e81da95c9
jaason-shi <jasons8002@gmail.com>
2024-04-03 20:27:14 -0700

    refactor code to dynamically populate the about page

--- frontend/src/components/About/Sidebar.jsx
+++ frontend/src/components/About/Sidebar.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Sidebar = () => {
+  const [selectedItem, setSelectedItem] = useState("About DSD");
+
+  const handleItemClick = (itemName) => {
+    setSelectedItem(itemName);
+  };
+
   return (
     <div className="col-md-3 mt-4">
       <div className="list-group">
         <a
           href="#"
-          className="list-group-item list-group-item-action active"
-          aria-current="true"
+          className={`list-group-item list-group-item-action ${
+            selectedItem === "About DSD" ? "active" : ""
+          }`}
+          onClick={() => handleItemClick("About DSD")}
         >
           About DSD
         </a>
-        <a href="#" className="list-group-item list-group-item-action">
+        <a
+          href="#"
+          className={`list-group-item list-group-item-action ${
+            selectedItem === "Using This Website" ? "active" : ""
+          }`}
+          onClick={() => handleItemClick("Using This Website")}
+        >
           Using This Website
         </a>
-        <a href="#" className="list-group-item list-group-item-action">
+        <a
+          href="#"
+          className={`list-group-item list-group-item-action ${
+            selectedItem === "Acknowledgements" ? "active" : ""
+          }`}
+          onClick={() => handleItemClick("Acknowledgements")}
+        >
           Acknowledgements
         </a>
-        <a href="#" className="list-group-item list-group-item-action">
+        <a
+          href="#"
+          className={`list-group-item list-group-item-action ${
+            selectedItem === "Contact" ? "active" : ""
+          }`}
+          onClick={() => handleItemClick("Contact")}
+        >
           Contact
         </a>
       </div>
