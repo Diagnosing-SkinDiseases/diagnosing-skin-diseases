5f353c95770b0f64ed9a9ea4053e00a2eb955659
Olga Zimina <olga.vic.zimina@gmail.com>
2024-04-08 10:03:43 -0700

    Add docs for Save, Publish, Preview controls

--- frontend/src/components/AdminDashboard/EditorComponents/EditorButtons.jsx
+++ frontend/src/components/AdminDashboard/EditorComponents/EditorButtons.jsx
@@ -3,6 +3,14 @@ import labels from "../labels.json";
 import Button from "../GeneralComponents/Button";
 import "../styles/Controls.css"; 
 
+/**
+ * Generates editor buttons for preview, save, and publish actions.
+ *
+ * @param {function} onPreview - Function to handle preview action
+ * @param {function} onSave - Function to handle save action
+ * @param {function} onPublish - Function to handle publish action
+ * @return {JSX.Element} Editor buttons component
+ */
 const EditorButtons = ({ onPreview, onSave, onPublish }) => {
   return (
     <div className="buttons">
