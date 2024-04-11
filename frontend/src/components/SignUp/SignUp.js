d64cf911c226cfa5be6a36bbc0a71b2dfc3dab12
Emily Tran <Emily.tran.1995@gmail.com>
2024-04-11 15:25:43 -0700

    Add SignUp docs

--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -3,22 +3,54 @@ import { apiCreateUser } from "../../apiControllers/userApiController";
 import SignUpForm from "./SignUpForm";
 import "../CSS/SignUp.css";
 
+/**
+ * App Component
+ *
+ * This is the main component of the application's signup page. It handles the registration of
+ * a new user through the SignUpForm component. It interacts with the backend via the `apiCreateUser`
+ * function which sends a POST request to create a new user. This component also manages the
+ * state related to server errors which might occur during the signup process.
+ *
+ * State:
+ *   serverError (String): Message indicating errors returned by the server during the signup process.
+ *
+ * Behavior:
+ *   - Provides `addUser` function to SignUpForm as a prop.
+ *   - Captures and displays server-side errors.
+ *   - Manages API interactions for creating a new user.
+ */
 function App() {
   const [serverError, setServerError] = useState("");
 
+  /**
+   * App Component
+   *
+   * This is the main component of the application's signup page. It handles the registration of
+   * a new user through the SignUpForm component. It interacts with the backend via the `apiCreateUser`
+   * function which sends a POST request to create a new user. This component also manages the
+   * state related to server errors which might occur during the signup process.
+   *
+   * State:
+   *   serverError (String): Message indicating errors returned by the server during the signup process.
+   *
+   * Behavior:
+   *   - Provides `addUser` function to SignUpForm as a prop.
+   *   - Captures and displays server-side errors.
+   *   - Manages API interactions for creating a new user.
+   */
   const addUser = async (user) => {
-    setServerError("");
+    setServerError(""); // Clear any existing errors before a new request
     try {
-      // Using Axios for the POST request
       const response = await apiCreateUser(user);
       console.log("User created:", response.data);
       // Handle success (e.g., display success message, clear form, etc.)
     } catch (error) {
+      // Construct a user-friendly error message based on the server's response
       const errorMessage =
         error.response && error.response.data && error.response.data.message
           ? error.response.data.message
           : "An error occurred during sign-up.";
-      setServerError(errorMessage);
+      setServerError(errorMessage); // Update state with the new error message
     }
   };
 
