976a23195d2603865971d966af0d4216d56396b5
Sean Sollestre <sean.sollestre@gmail.com>
2024-04-12 10:02:40 -0700

    Add jsdocss for loading

--- frontend/src/components/Loading/LoadingPage.jsx
+++ frontend/src/components/Loading/LoadingPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "../CSS/Loading.css";
 
+/**
+ * LoadingPage component renders a loading indicator while content is being fetched.
+ * @returns {JSX.Element} - Returns the JSX element for the loading indicator.
+ */
 const LoadingPage = () => {
   return (
     <>
       <div className="loading-container">
+        {/* Loading indicator */}
         <h1>Loading...</h1>
         <p>Please wait as we fetch your content.</p>
       </div>
