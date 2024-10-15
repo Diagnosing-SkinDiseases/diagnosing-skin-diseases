809f523240f1af00f653b30c229ee21ef9d2c7be
Olga Zimina <olga.vic.zimina@gmail.com>
2024-10-15 12:00:45 -0700

    Render ContactForm based on articleId

--- frontend/src/components/About/MainContent.jsx
+++ frontend/src/components/About/MainContent.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import Article from "../Article/Article";
+import ContactForm from "./ContactForm"; // Import your ContactForm component
 
 const MainContent = ({ articleId }) => {
+  // If the articleId is "contact", display the contact form
+  if (articleId === "contact") {
+    return (
+      <div className="col-md-9">
+        <ContactForm />
+      </div>
+    );
+  }
+
+  // Otherwise, display the article
   return (
     <div className="col-md-9">
       <Article articleId={articleId}></Article>
