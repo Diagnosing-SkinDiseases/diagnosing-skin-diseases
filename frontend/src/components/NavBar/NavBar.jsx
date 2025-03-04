e08fcecb3a34108410d3f26b9509b175115db430
SSollestre <sean.sollestre@gmail.com>
2025-03-04 11:56:53 -0800

    Switch to new sent navbar logo

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../App/AuthContext";
-import DSD from "../NavBar/DSD.png";
+// import DSD from "../NavBar/DSD.png";
+import DSD from "../NavBar/DSD_Text_Only.png";
 import "../CSS/NavBarComponent.css";
 
 /**
