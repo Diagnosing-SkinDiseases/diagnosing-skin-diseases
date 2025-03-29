024e44d2c7e63bb7fcbb5cd9a6dcbf2413984bab
SSollestre <sean.sollestre@gmail.com>
2025-03-29 12:18:47 -0700

    Add content container for strange footer merge edge case

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -37,199 +37,204 @@ function App() {
     <AuthProvider>
       <div className="app-container">
         <NavBarComponent></NavBarComponent>
-        <Routes>
-          {/* Admin */}
-          {/* Admin - Trees */}
-          <Route
-            path="/admin/trees"
-            element={
-              <ProtectedRoute>
-                <AdminDashboard contentType={ContentTypeEnum.TREE} />
-              </ProtectedRoute>
-            }
-          ></Route>
+        <div className="content-container">
+          <Routes>
+            {/* Admin */}
+            {/* Admin - Trees */}
+            <Route
+              path="/admin/trees"
+              element={
+                <ProtectedRoute>
+                  <AdminDashboard contentType={ContentTypeEnum.TREE} />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Trees - Add */}
-          <Route
-            path="/admin/trees/add"
-            element={
-              <ProtectedRoute>
-                <ContentEditor contentType={ContentTypeEnum.TREE} />
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Trees - Add */}
+            <Route
+              path="/admin/trees/add"
+              element={
+                <ProtectedRoute>
+                  <ContentEditor contentType={ContentTypeEnum.TREE} />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Trees - Edit */}
-          <Route
-            path="/admin/trees/edit/:id"
-            element={
-              <ProtectedRoute>
-                <ContentEditor
-                  contentType={ContentTypeEnum.TREE}
-                ></ContentEditor>
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Trees - Edit */}
+            <Route
+              path="/admin/trees/edit/:id"
+              element={
+                <ProtectedRoute>
+                  <ContentEditor
+                    contentType={ContentTypeEnum.TREE}
+                  ></ContentEditor>
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Trees - Preview */}
-          <Route
-            path="/admin/trees/preview"
-            element={
-              <ProtectedRoute>
-                {/* <BuildUserTree></BuildUserTree> */}
-                <UserTreeV2></UserTreeV2>
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Trees - Preview */}
+            <Route
+              path="/admin/trees/preview"
+              element={
+                <ProtectedRoute>
+                  {/* <BuildUserTree></BuildUserTree> */}
+                  <UserTreeV2></UserTreeV2>
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Articles */}
-          <Route
-            path="/admin/articles"
-            element={
-              <ProtectedRoute>
-                <AdminDashboard contentType={ContentTypeEnum.ARTICLE} />
-              </ProtectedRoute>
-            }
-          ></Route>
-          {/* Admin - Articles - Add */}
-          <Route
-            path="/admin/articles/add"
-            element={
-              <ProtectedRoute>
-                <ContentEditor contentType={ContentTypeEnum.ARTICLE} />
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Articles */}
+            <Route
+              path="/admin/articles"
+              element={
+                <ProtectedRoute>
+                  <AdminDashboard contentType={ContentTypeEnum.ARTICLE} />
+                </ProtectedRoute>
+              }
+            ></Route>
+            {/* Admin - Articles - Add */}
+            <Route
+              path="/admin/articles/add"
+              element={
+                <ProtectedRoute>
+                  <ContentEditor contentType={ContentTypeEnum.ARTICLE} />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Articles - Edit */}
-          <Route
-            path="/admin/articles/edit/:id"
-            element={
-              <ProtectedRoute>
-                <ContentEditor contentType={ContentTypeEnum.ARTICLE} />
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Articles - Edit */}
+            <Route
+              path="/admin/articles/edit/:id"
+              element={
+                <ProtectedRoute>
+                  <ContentEditor contentType={ContentTypeEnum.ARTICLE} />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Articles - Preview */}
-          <Route
-            path="admin/articles/preview"
-            element={
-              <ProtectedRoute>
-                <Article />
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Articles - Preview */}
+            <Route
+              path="admin/articles/preview"
+              element={
+                <ProtectedRoute>
+                  <Article />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Glossary */}
+            {/* Admin - Glossary */}
 
-          <Route
-            path="/admin/definitions"
-            element={
-              <ProtectedRoute>
-                <AdminDashboard contentType={ContentTypeEnum.DEFINITION} />
-              </ProtectedRoute>
-            }
-          ></Route>
+            <Route
+              path="/admin/definitions"
+              element={
+                <ProtectedRoute>
+                  <AdminDashboard contentType={ContentTypeEnum.DEFINITION} />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Glossary - Add */}
-          <Route
-            path="/admin/definitions/add"
-            element={
-              <ProtectedRoute>
-                <ContentEditor contentType={ContentTypeEnum.DEFINITION} />
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Glossary - Add */}
+            <Route
+              path="/admin/definitions/add"
+              element={
+                <ProtectedRoute>
+                  <ContentEditor contentType={ContentTypeEnum.DEFINITION} />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Glossary - Edit */}
-          <Route
-            path="/admin/definitions/edit/:id"
-            element={
-              <ProtectedRoute>
-                <ContentEditor contentType={ContentTypeEnum.DEFINITION} />
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Glossary - Edit */}
+            <Route
+              path="/admin/definitions/edit/:id"
+              element={
+                <ProtectedRoute>
+                  <ContentEditor contentType={ContentTypeEnum.DEFINITION} />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* Admin - Glossary - Preview */}
-          <Route
-            path="admin/definitions/preview"
-            element={
-              <ProtectedRoute>
-                <Glossary />
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/* Admin - Glossary - Preview */}
+            <Route
+              path="admin/definitions/preview"
+              element={
+                <ProtectedRoute>
+                  <Glossary />
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* User Routes */}
-          {/* Homepage */}
-          <Route path="/" element={<Homepage></Homepage>}></Route>
+            {/* User Routes */}
+            {/* Homepage */}
+            <Route path="/" element={<Homepage></Homepage>}></Route>
 
-          {/* User - Trees */}
-          <Route path="/trees" element={<UserTree></UserTree>}></Route>
+            {/* User - Trees */}
+            <Route path="/trees" element={<UserTree></UserTree>}></Route>
 
-          {/* User - Trees - White Macules */}
-          <Route
-            path="/trees/:id"
-            element={
-              // <BuildUserTree />
-              <UserTreeV2 />
-            }
-          ></Route>
+            {/* User - Trees - White Macules */}
+            <Route
+              path="/trees/:id"
+              element={
+                // <BuildUserTree />
+                <UserTreeV2 />
+              }
+            ></Route>
 
-          {/* User - Trees - OldVer */}
-          <Route
-            path="v1/trees/:id"
-            element={
-              <BuildUserTree />
-              // <UserTreeV2 />
-            }
-          ></Route>
+            {/* User - Trees - OldVer */}
+            <Route
+              path="v1/trees/:id"
+              element={
+                <BuildUserTree />
+                // <UserTreeV2 />
+              }
+            ></Route>
 
-          {/* Article List */}
-          <Route path="/treatment" element={<ArticlePage />}></Route>
+            {/* Article List */}
+            <Route path="/treatment" element={<ArticlePage />}></Route>
 
-          {/* Article */}
-          <Route path="/treatment/:title/:id" element={<Article />}></Route>
+            {/* Article */}
+            <Route path="/treatment/:title/:id" element={<Article />}></Route>
 
-          {/* Glossary */}
-          <Route path="/glossary" element={<Glossary></Glossary>}></Route>
+            {/* Glossary */}
+            <Route path="/glossary" element={<Glossary></Glossary>}></Route>
 
-          {/* How To */}
-          <Route path="/how-to/:title/:id" element={<Article />}></Route>
-          <Route
-            path="/how-to-understand-skin/:title/:id"
-            element={<Article />}
-          ></Route>
-          <Route
-            path="/how-to-diagnose/:title/:id"
-            element={<Article />}
-          ></Route>
-          <Route path="/how-to-treat/:title/:id" element={<Article />}></Route>
+            {/* How To */}
+            <Route path="/how-to/:title/:id" element={<Article />}></Route>
+            <Route
+              path="/how-to-understand-skin/:title/:id"
+              element={<Article />}
+            ></Route>
+            <Route
+              path="/how-to-diagnose/:title/:id"
+              element={<Article />}
+            ></Route>
+            <Route
+              path="/how-to-treat/:title/:id"
+              element={<Article />}
+            ></Route>
 
-          {/*Login*/}
-          <Route path="/login" element={<Login></Login>}></Route>
+            {/*Login*/}
+            <Route path="/login" element={<Login></Login>}></Route>
 
-          {/*Logout*/}
-          <Route path="/logout" element={<Logout />}></Route>
+            {/*Logout*/}
+            <Route path="/logout" element={<Logout />}></Route>
 
-          {/*Signup*/}
-          <Route
-            path="/signup"
-            element={
-              <ProtectedRoute>
-                <Signup></Signup>
-              </ProtectedRoute>
-            }
-          ></Route>
+            {/*Signup*/}
+            <Route
+              path="/signup"
+              element={
+                <ProtectedRoute>
+                  <Signup></Signup>
+                </ProtectedRoute>
+              }
+            ></Route>
 
-          {/* About DSD */}
-          <Route path="/about" element={<About />}></Route>
+            {/* About DSD */}
+            <Route path="/about" element={<About />}></Route>
 
-          {/* 404 */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+            {/* 404 */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
         {/* <div className="footer-placeholder"></div> */}
         <footer className="footer">
           © 2024 Diagnosing Skin Diseases and Dr. Eileen Murray.
