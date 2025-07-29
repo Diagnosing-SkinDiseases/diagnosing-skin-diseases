e6cd5da1411315008bfd3b923fba88df90f52838
SSollestre <sean.sollestre@gmail.com>
2025-07-29 13:37:13 -0700

    Fix preview not resetting on new ImageContent input

--- frontend/src/components/AdminDashboard/EditorComponents/ImageInput.jsx
+++ frontend/src/components/AdminDashboard/EditorComponents/ImageInput.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Button from '../GeneralComponents/Button';
-import "../../CSS/Admin/List.css"; 
+import React, { useState, useEffect, useRef } from "react";
+import Button from "../GeneralComponents/Button";
+import "../../CSS/Admin/List.css";
 import labels from "../labels.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
@@ -8,27 +8,32 @@ import { faUpload, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 /**
  * * ImageInput is a React component for uploading, previewing, and removing images within a form or content block.
  * It provides a user interface for selecting an image file, displaying a preview of the image, and allowing the
- * user to remove the image. 
- * 
+ * user to remove the image.
+ *
  * @param {Object} props.block - The content block object with a 'value' property for the image.
  * @param {Function} props.updateBlock - Callback to update the block's value with the selected image.
  * @param {Function} props.remove - Callback to remove the image from the block.
  * @returns {JSX.Element} The rendered ImageInput component.
  */
 const ImageInput = ({ block, updateBlock, remove }) => {
-  const [preview, setPreview] = useState('');
+  const [preview, setPreview] = useState("");
   const fileInputRef = useRef();
 
   /**
    * Setts the preview of the image to the selected image.
-   * 
+   *
    * @listens block.value - Watches for changes in the block's value to update the image preview accordingly.
    */
   useEffect(() => {
     // If block.value is a base64 string and starts with 'data:image/', set it as the preview
-    if (block.value && typeof block.value === 'string' && block.value.startsWith('data:image')) {
+    if (
+      block.value &&
+      typeof block.value === "string" &&
+      block.value.startsWith("data:image")
+    ) {
       setPreview(block.value);
-
+    } else {
+      setPreview("");
     }
   }, [block.value]);
 
@@ -43,12 +48,11 @@ const ImageInput = ({ block, updateBlock, remove }) => {
       const reader = new FileReader();
       reader.onloadend = () => {
         setPreview(reader.result);
-        updateBlock(reader.result );
+        updateBlock(reader.result);
       };
       reader.readAsDataURL(file);
     }
   };
-
 
   /**
    * Triggers the file input
@@ -63,24 +67,30 @@ const ImageInput = ({ block, updateBlock, remove }) => {
   return (
     <div className="image-input-container">
       {preview ? (
-        <img src={preview} alt="Preview" className="image-preview" onClick={handlePreviewClick} />
+        <img
+          src={preview}
+          alt="Preview"
+          className="image-preview"
+          onClick={handlePreviewClick}
+        />
       ) : (
         <label className="image-upload-label" onClick={handlePreviewClick}>
-            <FontAwesomeIcon icon={faUpload} className="fa-upload-icon" />
-            <div className="upload-label-text">{labels.imageLabel}</div>
+          <FontAwesomeIcon icon={faUpload} className="fa-upload-icon" />
+          <div className="upload-label-text">{labels.imageLabel}</div>
         </label>
       )}
       <input
         type="file"
         accept="image/*"
         onChange={handleImageChange}
-        style={{ display: 'none' }}
-        ref={fileInputRef}/>
-        <Button onClick={remove} className="delete-button">
-            <FontAwesomeIcon icon={faTrashAlt} />
-        </Button>
+        style={{ display: "none" }}
+        ref={fileInputRef}
+      />
+      <Button onClick={remove} className="delete-button">
+        <FontAwesomeIcon icon={faTrashAlt} />
+      </Button>
     </div>
-    );
+  );
 };
 
 export default ImageInput;
